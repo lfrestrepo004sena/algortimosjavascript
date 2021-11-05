7e4b23c555c6055d41552a369dfe86df52dfb8e8
lfrestrepo004sena <70179712+lfrestrepo004sena@users.noreply.github.com>
2021-11-05 14:32:36 -0500

    Add files via upload

---
+++ algoritmos.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <a id="logo" href="index.html"> <img src="img/logo.gif" alt="Logo"> </a>
+            <h1>ARCHITECTURE</h1>
+            <nav>
+                <ul>
+                    <li> <a href="index.html">HOME</a> </li>
+                    <li> <a href="about.html">ABOUT</a> </li>
+                    <li> <a class="active" href="services.html">SERVICES</a> </li>
+                    <li> <a href="projects.html">PROJECTS</a> </li>
+                    <li> <a href="blog.html">BLOG</a> </li>
+                    <li> <a href="contact.html">CONTACT</a> </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <div class="socials">
+            <div class="container">
+                <h2>GET IN TOUCH</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maurissemper eu turpis sed, mollis vehicula
+                    neque. Ut in<br>urna ante. Etiam sit amet arcu ante.</p>
+                <ul>
+                    
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://es-la.facebook.com/" target="_blank">
+                            <i class="fab fa-facebook"></i>
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-pinterest"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://wa.link/7mz8nf" target="_blank">
+                            <i class="fab fa-whatsapp"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="copy">
+            <p>ARCHITECTURE © 2021 | <a href="#">PRIVACY POLICY</a></p>
+        </div>
+    </footer>
+</body>
+
+</html>
